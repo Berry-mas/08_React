d4c2beef0af8e7a9876c630e49c8a0bc8fd3d85a
junseo <junseo9145@gmail.com>
2025-05-14 15:47:12 +0900

    중간 커밋

--- 05_PARKJUNSEO/module08-redux-store/src/app/page.js
+++ 05_PARKJUNSEO/module08-redux-store/src/app/page.js
@@ -1,7 +1,36 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>hello world</h1>
+
+      <hr />
+
+      <h2>chap01-basic Redux</h2>
+      <ul>
+        <li>
+          <Link href="/chap01">basic Redux</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <h2>chap02-basic Redux</h2>
+      <ul>
+        <li>
+          <Link href="/chap02">combine Reducers</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <h2>chap03-Redux toolkit</h2>
+      <ul>
+        <li>
+          <Link href="/chap03">Redux toolkit</Link>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ 05_PARKJUNSEO/module08-redux-store/src/components/chap03/store.jsx
@@ -0,0 +1,9 @@
+// 저장소에 어떤 Reducer가 들어있는지 확인 가능
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
